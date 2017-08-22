dcf81db6601e0bbd85fbc1cb1b658e1dc518f30f
Guillermo Pincay <guillermaster@gmail.com>
2017-08-21 22:06:28 -0500

    RoomsGrid & RoomBox tests

--- src/components/containers/RoomsGrid/RoomsGrid.test.js
+++ src/components/containers/RoomsGrid/RoomsGrid.test.js
@@ -4,5 +4,6 @@ import RoomsGrid from './RoomsGrid';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<RoomsGrid />, div);
+  const rooms = '[{"id":7,"propertyTypeId":231,"propertyType":false,"name":"Deep Tracks 2","description":"Located just 200 m from the Hirafu Gondola, this 1 bedroom apartment is ideal for those seeking fresh tracks.","code":"DT02","baseRoom":"","gradeId":2,"gradeSort":1,"locationId":16,"accomTypeId":1,"viewId":9,"kitchenId":1,"liftDistanceId":179,"villageCentreDistanceId":182,"bathrooms":1,"standardPax":2,"maximumPax":3,"soldSeparately":true,"upgradedFacilities":false,"amenities":{"aircon":false,"appletv":false,"btspeakers":false,"cardkey":true,"chromecast":false,"fireplace":false,"hdtv":false,"jacuzzi":false,"nespresso":false},"bedConfigurations":[8,9,92],"status":"healthy","floorArea":44,"online":true,"images":[]}]';
+  ReactDOM.render(<RoomsGrid rooms={rooms} filter='' />, div);
 });
--- src/components/shared/RoomBox/RoomBox.test.js
+++ src/components/shared/RoomBox/RoomBox.test.js
@@ -4,5 +4,11 @@ import RoomBox from './RoomBox';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<RoomBox />, div);
+  ReactDOM.render(<RoomBox key={idx}
+    name="Deep Tracks 2"
+    description="Located just 200 m from the Hirafu Gondola, this 1 bedroom apartment is ideal for those seeking fresh tracks."
+    amenities={"aircon":false,"appletv":false,"btspeakers":false,"cardkey":true,"chromecast":false,"fireplace":false,"hdtv":false,"jacuzzi":false,"nespresso":false}
+    upgradedFacilities={false}
+    bathrooms={1}
+    />, div);
 });
