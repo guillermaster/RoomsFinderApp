2ef1d7f33e1d175811fc7ccb3b445cd50307f610
guillermaster <guillermaster@gmail.com>
2017-08-21 22:48:12 -0500

    fix RoomBox render unit test

--- src/components/shared/RoomBox/RoomBox.test.js
+++ src/components/shared/RoomBox/RoomBox.test.js
@@ -4,10 +4,10 @@ import RoomBox from './RoomBox';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<RoomBox key={idx}
+  ReactDOM.render(<RoomBox key={1}
     name="Deep Tracks 2"
     description="Located just 200 m from the Hirafu Gondola, this 1 bedroom apartment is ideal for those seeking fresh tracks."
-    amenities={"aircon":false,"appletv":false,"btspeakers":false,"cardkey":true,"chromecast":false,"fireplace":false,"hdtv":false,"jacuzzi":false,"nespresso":false}
+    amenities={{"aircon":false,"appletv":false,"btspeakers":false,"cardkey":true,"chromecast":false,"fireplace":false,"hdtv":false,"jacuzzi":false,"nespresso":false}}
     upgradedFacilities={false}
     bathrooms={1}
     />, div);
